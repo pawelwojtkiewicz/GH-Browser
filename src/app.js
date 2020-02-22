936f7f5185d578dbb7905cf027d0159e6939adfc
Pawel Wojtkiewicz <pawel.wojtkiewicz66@gmail.com>
2020-02-22 21:29:46 +0100

    create files structure

--- src/app.js
+++ src/app.js
@@ -1,58 +1,19 @@
 import './assets/scss/app.scss';
 import $ from 'cash-dom';
+import checkUserNameFieldCorrectness from './utilities/checkUserNameFieldCorrectness.js';
 
 
 export class App {
   initializeApp() {
-    let self = this;
-
-    const userNameCondition = "[a-z0-9-_]";
-    const reg = new RegExp(userNameCondition);
-
-    const checkUserNameFieldCorrectness = (reg, userName) => reg.test(userName);
-
-    const removeInputUserNameError = userNameInput => {
-      userNameInput.removeClass("is-danger");
-      userNameInput.off("input", () => removeInputUserNameError(userNameInput));
-    };
-
-    const setInputUserNameError = userNameInput => {
-      userNameInput.addClass("is-danger");
-      userNameInput.on("input", () => removeInputUserNameError(userNameInput));
-    };
-
-    var URL = 'https://api.github.com/users/';
-
-    const getData = userName => {
-      fetch(URL + userName)
-      .then((response)=> response.json())
-      .then(function (body) {
-        self.profile = body;
-        self.update_profile();
-      })
-    }
-
-    function getDataForIE(userName){
-      const xhr = new XMLHttpRequest();
-      xhr.addEventListener("load", function() {
-            self.profile = JSON.parse(xhr.response);
-            self.update_profile();
-      });
-      xhr.open("GET", URL + userName, true);
-      xhr.send();   
-    }
+    const self = this;
 
     $('.load-username').on('click', function () {
-      let userName = $('.username.input').val();
+      const userName = $('.username.input').val();
       const userNameInput = $('.username.input');
 
-      const checkFieldResult = checkUserNameFieldCorrectness(reg, userName);
-      if(checkFieldResult){
-        var isIE = /*@cc_on!@*/false || !!document.documentMode;
-        if(!isIE) getData(userName);
-        else getDataForIE(userName);
-      } else setInputUserNameError(userNameInput);
+      const isUserNameCorrect = checkUserNameFieldCorrectness(userName, userNameInput);
     })
+
   }
 
   update_profile() {
