1c5b6f5bb7e0a6a6975389857fa4b11cd159e6ce
Pawel Wojtkiewicz <pawel.wojtkiewicz66@gmail.com>
2020-02-23 18:07:01 +0100

    change file structure

--- src/components/userProfile/UserProfile-modal.js
+++ src/components/userProfile/UserProfile-modal.js
@@ -1,8 +1,66 @@
-import fetchUserInfo from '../../services/fetchUserInfo.js'
-
 export default class UserProfileModal {
+
+
+
+
     getUserData(userName){
-        const userInfo = fetchUserInfo(userName);
-        return userInfo;
+        const URLuserInformations = `https://api.github.com/users/${userName}`;
+        const URLuserHistory = `https://api.github.com/users/${userName}/events/public`;
+    
+        function checkIfFetchIsAvaible(){
+            if (!window.fetch) {
+               return false;
+            } else return true;
+        }
+    
+        function getDataByFetch(userName){
+            let userInformations;
+            let userHistory;
+    
+            function getUserHistory(){
+                fetch(URLuserInformations)
+                    .then(response => response.json())
+                    .then(function (body) {
+                        userInformatins = body;
+                });
+            }
+    
+            function getUserInformations(){
+                fetch(URLuserHistory)
+                    .then(response => response.json())
+                    .then(function (body) {
+                        userHistory = body;
+                });
+            }
+        }
+    
+        function getDataByXMLHttpRequest(userName){
+            let userInformations;
+            let userHistory;
+    
+            function getUserHistory(){
+                const xhr = new XMLHttpRequest();
+                xhr.addEventListener("load", function() {
+                    userHistory = JSON.parse(xhr.response);
+                });
+                xhr.open("GET", URLuserInformations, true);
+                xhr.send();
+            }
+    
+            function getUserInformations(){
+                const xhr = new XMLHttpRequest();
+                xhr.addEventListener("load", function() {
+                    userInformations = JSON.parse(xhr.response);
+                    getUserHistory();
+                });
+                xhr.open("GET", URLuserInformations, true);
+                xhr.send();
+            }
+    
+            getUserInformations();
+        }
+    
+        const isFetchAvaible = checkIfFetchIsAvaible();
+        isFetchAvaible ? getDataByFetch(userName) : getDataByXMLHttpRequest(userName);
     }
 }
