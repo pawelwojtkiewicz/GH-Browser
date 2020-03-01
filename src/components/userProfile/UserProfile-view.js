b199685e5d196e61e536081bcb16a1ca1862c22f
Pawel Wojtkiewicz <pawel.wojtkiewicz66@gmail.com>
2020-03-01 12:23:59 +0100

    change create user informations

--- src/components/userProfile/UserProfile-view.js
+++ src/components/userProfile/UserProfile-view.js
@@ -1,48 +1,47 @@
 import $ from 'cash-dom';
-import createElement from '../../utilities/createElement.js'
 
 export default class UserProfileView {
     renderUserProfileInormations(userInformations){
-        const {login, avatar_url, html_url, bio} = userInformations.body;
-        const createProfileContainer = createElement("div", [["class", "profile"]]);
-        const createMediaContainer = createElement("div", [["class", "media"]]);
-
-        const createImgContainer = avatar_url => {
-            const container = createElement("div", [["class", "media-left"]]);
-            const createFigure = createElement("figure", [["class", "image is-64x64"]]);
-            const createImg = createElement("img", [["src", avatar_url], ["id", "profile-image"]]);
-            createFigure.appendChild(createImg);
-            container.appendChild(createFigure);
-            return container;
-        }
-        const createMediaContent = (login, html_url) => {
-            const container = createElement("div", [["class", "media-content"]]);
-            const userNameField = createElement("p", [["class", "title is-5"], ["id", "profile-name"]], login);
-            const userNameSubtitle = createElement("p", [["class", "subtitle is-6"]]);
-            const userNameLink = createElement("a", [["href", html_url], ["id", "profile-name"]], login);
-
-            userNameSubtitle.appendChild(userNameLink);
-            container.appendChild(userNameField);
-            container.appendChild(userNameSubtitle);
-            return container;
-        }
-        const createMedia = bio => {
-            const container = createElement("div", [["class", "content"], ["id", "profile-bio"]]);
-            const description = createElement("p", [], bio || '(no information)');
-
-            container.appendChild(description);
+        const createUserProfileInormationsContainer = children => {
+            const container = document.createElement("div");
+            container.className = "profile";
+            container.innerHTML = children;
             return container;
         }
 
-        const imgContainer = createImgContainer(avatar_url);
-        const mediaContent = createMediaContent(login, html_url);
-        const media = createMedia(bio);
+        const createContent = elementData => {
+            const {login, avatar_url, html_url, bio} = elementData;
+            return  (`
+                <div class="media">
+                    <div class="media-left">
+                        <figure class="image is-64x64">
+                            <img src="${avatar_url}" id="profile-image">
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-5" id="profile-name">
+                            ${login}
+                        </p>
+                        <p class="subtitle is-6">
+                            <a href="${html_url}" id="profile-name">
+                                @${login}
+                            </a>
+                        </p>
+                    </div>
+                    </div>
+                        <div class="content" id="profile-bio">
+                            <p>
+                               ${bio}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            `);
+        }
 
-        createMediaContainer.appendChild(imgContainer);
-        createMediaContainer.appendChild(mediaContent);
-        createProfileContainer.appendChild(createMediaContainer);
-        createProfileContainer.appendChild(media);
-        $('.profile').replaceWith(createProfileContainer);
+        const elementContent = createContent(userInformations.body);
+        const elementContainer = createUserProfileInormationsContainer(elementContent);
+        $('.profile').replaceWith(elementContainer);
     }
 
     renderUserProfileTimeline(userHistory){
