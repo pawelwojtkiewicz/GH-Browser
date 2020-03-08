aae8d6d223ad72515d28e7c35b2f9b5b1057cf97
Pawel Wojtkiewicz <pawel.wojtkiewicz66@gmail.com>
2020-03-08 16:16:18 +0100

    change position of validate user name

--- src/app.js
+++ src/app.js
@@ -1,20 +1,44 @@
+import "./assets/scss/app.scss";
 import './assets/scss/app.scss';
 import 'promise-polyfill/src/polyfill';
 import 'whatwg-fetch'
 import $ from 'cash-dom';
-import UserProfile from './components/userProfile';
-import checkUserNameFieldCorrectness from './utilities/checkUserNameFieldCorrectness.js';
+import UserProfile from "./components/userProfile";
+
+const removeInputUserNameError = userNameInput => {
+  userNameInput.removeClass("is-danger");
+  userNameInput.off();
+};
+
+const setInputUserNameError = userNameInput => {
+  userNameInput.addClass("is-danger");
+  userNameInput.on("input", () => removeInputUserNameError(userNameInput));
+}
+
+const userFieldRegex = new RegExp("[a-z0-9-_]+$");
+
+const validateUserName = userName => {
+  return new Promise((resolve, reject) => {
+    if (userFieldRegex.test(userName)) {
+      resolve(userName);
+    } else {
+      reject(Error("Invalid User Name"));
+    }
+  });
+}
 
 export class App {
   initializeApp(){
-    const userProfile = new UserProfile();
-
-    $('.load-username').on('click', function () {
-      const userNameInput = $('.username.input');
-      const userName = userNameInput.val();
-
-      const isUserNameCorrect = checkUserNameFieldCorrectness(userName, userNameInput);
-      if(isUserNameCorrect) userProfile.render(userName);
-    })
+    $(".load-username").on("click", function() {
+      const userNameInput = $(".username.input");
+      validateUserName(userNameInput.val())
+        .then(userName => {
+          new UserProfile().render(userName);
+        })
+        .catch(error => {
+          console.error(`Error ${error}`);
+          setInputUserNameError(userNameInput);
+        });
+    });
   }
 }
